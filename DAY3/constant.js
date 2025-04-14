8af17609e67c92ec69dc94bfeb03aaa8fa7a2db4
HariniDhanalakshmi <harininatarajan2005@gmail.com>
2025-04-14 19:19:28 +0530

    feat: added  file

---
+++ DAY3/constant.js
@@ -0,0 +1,10 @@
+const UNAME = "learn@outlook.in";
+const PWD = "thisispassword";
+
+const WHITE_COLOUR = "#fff";
+const BLACK_COLOUR = "#000";
+
+console.log(UNAME, PWD);
+
+const DATE_OF_BIRTH = '21.03.2005';
+const age = calAge(DATE_OF_BIRTH);
