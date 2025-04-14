e88ae7fecd8610b77c97a8dd8754aff63af47589
HariniDhanalakshmi <harininatarajan2005@gmail.com>
2025-04-14 16:43:03 +0530

    feat: added variables file

---
+++ DAY3/variables.js
@@ -0,0 +1,21 @@
+// Variable declaration
+var bookName, sum, diff;
+let bookName$;
+bookName$ = "hps";
+// variable initialization
+bookName = "Harry Potter";
+
+let bookAuthor = "J.K. Rowling",
+    bookPublisher = "By me";
+    bookPart = "Prisoner of Askaban";
+    bookName = "HP";
+    bookName = bookAuthor;
+
+console.log(bookName, bookAuthor, "\t", bookPart);
+
+/* Rules to name a variable
+    - unique
+    $,_,use digits but not in starting  
+    keyword not allowed
+    case sensitive
+*/
